7bf19fc41c1a6de96bd852f238f499fe3ea34018
Ma-Luisa27 <fernandes.luisa@escolar.ifrn.edu.br>
2026-03-26 12:09:52 +0000

    Atualização - 26-03-2026

--- app.py
+++ app.py
@@ -11,7 +11,20 @@ def index():
 @app.route('/email')
 @app.route('/contato')
 def contato():
-    return render_template ('contato.html')
+    dados = {'nome': 'Italo', 'email': 'italo@email.com'}
+    return render_template ('contato.html', dados=dados)
+
+@app.route('/usuario', defaults={'nome': 'Desconhecido', 'sobrenome':'Desconhecido'})
+@app.route('/usuario/<nome>/<sobrenome>')
+def usuario (nome, sobrenome):
+    info = {'nome':nome, 'sobrenome':sobrenome}
+    return render_template('usuario.html', info=info)
+
+@app.route('/semestre/<int:x>')
+def semestre (x):
+    atual = x
+    anterior = x-1
+    return render_template('semestre.html', atual=atual, anterior=anterior)
 
 if __name__ == '__main__':
     app.run()
