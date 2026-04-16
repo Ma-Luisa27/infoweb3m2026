e7b86a654bc91615677879b835f777804c780e13
Ma-Luisa27 <fernandes.luisa@escolar.ifrn.edu.br>
2026-04-16 12:03:03 +0000

    Atualizações_16-04-26

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -29,6 +30,21 @@ def semestre (x):
 @app.route('/perfil/<usuario>')
 def perfil (usuario):
     return render_template('perfil.html', usuario=usuario)
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('/recebedados', methods=['POST'])
+def recebedados():
+    nome = request.form['nome']
+    sobrenome = request.form['sobrenome']
+    email = request.form['email']
+    datanasc = request.form['datanasc']
+    data_objeto = datetime.strptime(datanasc, "%Y-%m-%d")
+    data_formatada = data_objeto.strftime("%d-%m-%Y")
+    # info = request.args
+    return render_template('recebedados.html', nome=nome, sobrenome=sobrenome, email=email, data_formatada=data_formatada)
 
 if __name__ == '__main__':
     app.run()
